dbf01472fdef6b1fe6e6c409daca4511e61bd4e8
Eyan Tsai <eyan505@gmail.com>
2021-05-29 15:47:24 +0800

    error fix

--- js/products.js
+++ js/products.js
@@ -4,16 +4,17 @@ const productNum = document.querySelector("#productCount");
 
 const app = {
     data: {
-        product: [],
+        products: [],
     },
     getData(){
         const url = `${Url}/api/${path}/admin/products`;
         axios.get(url).then(res =>{
+            console.log(res);
             if (res.data.success){
                 this.data.products = res.data.products;
                 this.render();
             }else{
-                alert('Please Login again ');
+                alert('請重新登入 ');
                 window.location = 'login.html';
             }
         }).catch((error) => {
@@ -27,7 +28,7 @@ const app = {
       this.data.products.forEach((item) => {
           template += `
            <tr>
-                <td>${item.item}</td>    
+                <td>${item.title}</td>    
                 <td class="text-center" width="120">
                     ${parseInt(item.origin_price).toLocaleString()}    
                 </td>
@@ -35,10 +36,13 @@ const app = {
                     ${parseInt(item.price).toLocaleString()}
                 </td>  
                 <td class="text-center" width="100">
-                   <span class="${item.is_enabled ? 'text-success' : 'text-secondary'}">${item.is_enabled ? '啟用' : '未啟用'}</span> 
+                    <div class="form-check form-switch">
+                        <input class="form-check-input" type="checkbox" id="${item.id}" ${item.is_enabled? 'checked': ''} data-action="status" data-id="${item.id}">
+                        <label class="form-check-label" for="${item.id}">${item.is_enabled? '啟用' : '未啟用'}</label>
+                    </div>
                 </td>
                 <td class="text-center" width="120">
-                    <button type="button" class=" btn btn-sm btn-outline-danger move deleteBtn" data-action="remove" data-is="${item.id}">刪除</button>
+                <button type="button" class="btn btn-sm btn-outline-danger move deleteBtn" data-action="remove" data-id="${item.id}"> 刪除 </button>
                 </td>
             </tr>
             `;
@@ -46,20 +50,30 @@ const app = {
             productList.innerHTML = template;
             const deleteBtn = document.querySelectorAll('.deleteBtn');
             deleteBtn.forEach(btn =>{
-                btn.addEventListener('click,this.deleteProduct');
+                btn.addEventListener('click',this.deleteProduct.bind(this));
             })
       })
     },
     deleteProduct(e){
         const id = e.target.dataset.id;
-        axios.delete(`${Url}/api/${path}/admin/product/${id}`).then(res =>{
-            app.getData();
+        axios.delete(`${Url}/api/${path}/admin/product/${id}`)
+        .then(res =>{
+          console.log(res);
+          app.getData();
         });
+        
     },
     init(){
         const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/, '$1');(/(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
         axios.defaults.headers.common.Authorization = token;
         this.getData();
-    }
+    },
+    /*enableSate(id){
+        this.data.products.forEach((item) => {
+            if(id == item.id){
+                item.is_enabled = !item.is_enabled;
+            }
+        })
+    }*/
 }
 app.init();
